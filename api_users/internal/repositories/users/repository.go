7c8486a0ee52cc0e0b2a384b1279d65e4a468c94
walid bettahar <walidbettahar18@gmail.com>
2023-12-16 16:34:28 +0100

    Add repository for GET requests

---
+++ api_users/internal/repositories/users/repository.go
@@ -0,0 +1,51 @@
+package users
+
+import (
+	"users/internal/helpers"
+	"users/internal/models"
+
+	"github.com/gofrs/uuid"
+)
+
+func GetAllUsers() ([]models.User, error) {
+	db, err := helpers.OpenDB()
+	if err != nil {
+		return nil, err
+	}
+	rows, err := db.Query("SELECT * FROM users")
+	helpers.CloseDB(db)
+	if err != nil {
+		return nil, err
+	}
+
+	// parsing datas in object slice
+	users := []models.User{}
+	for rows.Next() {
+		var data models.User
+		err = rows.Scan(&data.ID, &data.Username, &data.Email)
+		if err != nil {
+			return nil, err
+		}
+		users = append(users, data)
+	}
+	// don't forget to close rows
+	_ = rows.Close()
+
+	return users, err
+}
+
+func GetUserById(id uuid.UUID) (*models.User, error) {
+	db, err := helpers.OpenDB()
+	if err != nil {
+		return nil, err
+	}
+	row := db.QueryRow("SELECT * FROM users WHERE id=?", id.String())
+	helpers.CloseDB(db)
+
+	var user models.User
+	err = row.Scan(&user.ID, &user.Username, &user.Email)
+	if err != nil {
+		return nil, err
+	}
+	return &user, err
+}
